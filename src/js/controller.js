0e12bbb7b811be4dbf0ec4d8cae1a532eab98301
MMRedFloyd <mirza-m97@hotmail.com>
2022-09-17 15:28:43 +0200

    New feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -129,5 +129,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
   console.log('Welcome');
+  aler('hacked');
 };
 init();
